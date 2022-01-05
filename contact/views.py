16dd92bc5b5b9fdb02a012fadf9ed04bf35aaba7
KyleCook97 <97108749+KyleCook97@users.noreply.github.com>
2022-01-04 22:29:05 -0500

    Rewrote some comments to become more clear

--- contact/views.py
+++ contact/views.py
@@ -4,17 +4,25 @@ from .models import Contact
 
 # Create your views here.
 
+#@login_required will not allow a user to access the index page without being logged in
 @login_required
 def index(request):
     contacts = Contact.objects.all()
+
+    #input from the search bar is stored then used to sort through the list of contacts
     search_input = request.GET.get('search-area')
+
+    #display all contacts that contain the searched string
     if search_input:
         contacts = Contact.objects.filter(full_name__icontains = search_input)
+
+    #if the search bar is empty, display all contacts
     else:
         contacts = Contact.objects.all()
         search_input = ''
     return render(request, 'index.html', {'contacts': contacts, 'search_input': search_input})
 
+#creates new contact then redirects user back to index page
 def addContact(request):
     if request.method == 'POST':
         new_contact = Contact(
@@ -28,13 +36,16 @@ def addContact(request):
         return redirect('/')
     return render(request, 'new.html')
 
+#loads the contact page for the id of pk, where pk is an int
 def contactProfile(request, pk):
     contact = Contact.objects.get(id=pk)
     return render(request, 'contact-profile.html', {'contact': contact})
 
+#user can edit a preexisting contact or delete a contact with an id = pk
 def editContact(request, pk):
     contact = Contact.objects.get(id=pk)
 
+    #once the new information is added in, redirect the user back to the contact profile page
     if request.method == 'POST':
         contact.full_name = request.POST['fullname']
         contact.relationship = request.POST['relationship']
@@ -46,6 +57,7 @@ def editContact(request, pk):
         return redirect('/profile/' + str(contact.id))
     return render(request, 'edit.html', {'contact': contact})
 
+#takes the user to the delete page to confirm if the contact should be deleted 
 def deleteContact(request, pk):
     contact = Contact.objects.get(id=pk)
 
